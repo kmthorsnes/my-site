cb166afedb7b0a0d0e168318f770dbf585613f67
kalle <kmthorsnes@gmail.com>
2018-04-03 23:19:18 +0200

    remove config

--- config.rb
+++ config.rb
@@ -26,3 +26,13 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 
 Haml::TempleEngine.disable_option_validator!
+
+activate :deploy do |deploy|
+  deploy.method   = :sftp
+  deploy.host            = 'ssh.karlmagnus.no'
+  deploy.port            = 22
+  deploy.path            = '/srv/www/site'
+  # Optional Settings
+  deploy.user     = 'karlmagnus.no' # no default
+  deploy.password = 'S@7N9\3n' # no default
+end
